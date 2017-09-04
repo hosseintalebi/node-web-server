c7710d9fa1355bb91d85f7ee3a5ba3a66a01fb0a
hosseintalebi <htalebi@hotmail.com>
2017-09-04 15:48:08 -0600

    different ways to answer to a response

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.set('view engine', 'hbs')
+// use middleware
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   // res.send('<div><h1>Hello Express</h1></div>')
-  res.send({
-    name: 'Hoss',
-    likes: [
-      'bouldering',
-      'programming',
-    ]
+  // res.send({
+  //   name: 'Hoss',
+  //   likes: [
+  //     'bouldering',
+  //     'programming',
+  //   ]
+  // })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to this page',
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About page')
+  // res.send('About page')
+
+  // view directory is the default folder for templates that express look for
+  // that's why we don't specify the dir in the below render call
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
 })
 
 app.get('/bad', (req, res) => {
@@ -23,4 +46,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('server is up on localhost:3000')
+})
