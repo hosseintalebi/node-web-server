0c6cf60130a393d76da1d5075e9001ea9dd62822
hosseintalebi <htalebi@hotmail.com>
2017-09-04 17:00:25 -0600

    Uses express middlewares

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express()
 
+// register partials
 hbs.registerPartials(__dirname + '/views/partials')
+
+// register helpers
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 })
+
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase()
 })
 
 app.set('view engine', 'hbs')
-// use middleware
+
+// middlewares
+// middleware for using static contents
 app.use(express.static(__dirname + '/public'))
 
+// custom middleware
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+
+  // tells application that middleware is done
+  // without calling next nothing after this won't be executed
+  next()
+})
+
+// Serving different routes
 app.get('/', (req, res) => {
   // res.send('<div><h1>Hello Express</h1></div>')
   // res.send({
